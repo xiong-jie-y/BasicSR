530ca23fb2cb2b4a76374d8c0d8a1afd5bcabd94
xiong-jie-y <fwvillage@gmail.com>
2021-12-01 12:04:29 +0900

    Add pixel unshuffling.

--- basicsr/archs/srresnet_arch.py
+++ basicsr/archs/srresnet_arch.py
@@ -3,7 +3,7 @@ from torch.nn import functional as F
 
 from basicsr.utils.registry import ARCH_REGISTRY
 from .arch_util import ResidualBlockNoBN, default_init_weights, make_layer
-
+from .arch_util import default_init_weights, make_layer, pixel_unshuffle
 
 @ARCH_REGISTRY.register()
 class MSRResNet(nn.Module):
@@ -25,18 +25,18 @@ class MSRResNet(nn.Module):
     def __init__(self, num_in_ch=3, num_out_ch=3, num_feat=64, num_block=16, upscale=4):
         super(MSRResNet, self).__init__()
         self.upscale = upscale
+        if upscale == 2:
+            num_in_ch = num_in_ch * 4
+        elif upscale == 1:
+            num_in_ch = num_in_ch * 16
 
         self.conv_first = nn.Conv2d(num_in_ch, num_feat, 3, 1, 1)
         self.body = make_layer(ResidualBlockNoBN, num_block, num_feat=num_feat)
 
         # upsampling
-        if self.upscale in [2, 3]:
-            self.upconv1 = nn.Conv2d(num_feat, num_feat * self.upscale * self.upscale, 3, 1, 1)
-            self.pixel_shuffle = nn.PixelShuffle(self.upscale)
-        elif self.upscale == 4:
-            self.upconv1 = nn.Conv2d(num_feat, num_feat * 4, 3, 1, 1)
-            self.upconv2 = nn.Conv2d(num_feat, num_feat * 4, 3, 1, 1)
-            self.pixel_shuffle = nn.PixelShuffle(2)
+        self.upconv1 = nn.Conv2d(num_feat, num_feat * 4, 3, 1, 1)
+        self.upconv2 = nn.Conv2d(num_feat, num_feat * 4, 3, 1, 1)
+        self.pixel_shuffle = nn.PixelShuffle(2)
 
         self.conv_hr = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
         self.conv_last = nn.Conv2d(num_feat, num_out_ch, 3, 1, 1)
@@ -50,14 +50,18 @@ class MSRResNet(nn.Module):
             default_init_weights(self.upconv2, 0.1)
 
     def forward(self, x):
-        feat = self.lrelu(self.conv_first(x))
+        if self.upscale == 2:
+            pre_feat = pixel_unshuffle(x, scale=2)
+        elif self.upscale == 1:
+            pre_feat = pixel_unshuffle(x, scale=4)
+        else:
+            pre_feat = x
+
+        feat = self.lrelu(self.conv_first(pre_feat))
         out = self.body(feat)
 
-        if self.upscale == 4:
-            out = self.lrelu(self.pixel_shuffle(self.upconv1(out)))
-            out = self.lrelu(self.pixel_shuffle(self.upconv2(out)))
-        elif self.upscale in [2, 3]:
-            out = self.lrelu(self.pixel_shuffle(self.upconv1(out)))
+        out = self.lrelu(self.pixel_shuffle(self.upconv1(out)))
+        out = self.lrelu(self.pixel_shuffle(self.upconv2(out)))
 
         out = self.conv_last(self.lrelu(self.conv_hr(out)))
         base = F.interpolate(x, scale_factor=self.upscale, mode='bilinear', align_corners=False)
